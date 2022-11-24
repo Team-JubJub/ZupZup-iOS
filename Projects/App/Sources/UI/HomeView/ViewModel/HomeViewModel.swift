be1c637c008a1d23e2b12bdb31cb04c27bf8735e
luminoux <yungzin98@naver.com>
2022-11-24 21:35:46 +0900

    :sparkles: HomeVIewModel -> fetchData() 추가

--- Projects/App/Sources/UI/HomeView/ViewModel/HomeViewModel.swift
+++ Projects/App/Sources/UI/HomeView/ViewModel/HomeViewModel.swift
@@ -9,13 +9,32 @@
 import UIKit
 
 final class HomeViewModel {
-
-    var 
     
+    var stores: [Store]
     
-    
+    init(stores: [Store]) {
+        self.stores = stores
+    }
 }
 
-#if DEBUG
-
-#endif
+extension HomeViewModel {
+    private func fetchData() {
+        let item = Item(itemId: 1,
+                        storeId: 1,
+                        itemName: "영진 라뗴",
+                        price: 5000,
+                        discounted: 4000)
+        
+        let store = Store(id: 1,
+                          address: "부산광역시 장전동 123-48 1층",
+                          eventList: ["event", "event", "event"],
+                          merchandiseList: [item, item, item],
+                          name: "영진 상회",
+                          openTime: "09:00",
+                          event: "event")
+        
+        for _ in 0...3 {
+            self.stores.append(store)
+        }
+    }
+}
