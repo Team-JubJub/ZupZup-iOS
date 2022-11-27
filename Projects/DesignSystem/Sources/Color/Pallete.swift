2e08fbf1199bfaf818dc792dd05d7c30d7c9c6d7
luminoux <yungzin98@naver.com>
2022-11-27 15:25:28 +0900

    :sparkles: DesignSystem Pallete, Font 추가
    Palette : greyC8BCAB 추가
    Font : semiBold 추가, 15, 20 사이즈 추가

--- Projects/DesignSystem/Sources/Color/Pallete.swift
+++ Projects/DesignSystem/Sources/Color/Pallete.swift
@@ -7,21 +7,25 @@
 //
 
 public enum Pallete: String {
-    case greyAFA08A
-    case black1E1E1E
-    case greyEAE5DF
     case orangeE49318
+    case greyAFA08A
+    case greyEAE5DF
+    case greyC8BCAB
+    case black1E1E1E
+    
 
     var hexString: String {
         switch self {
-        case .greyAFA08A:
-            return "#AFA08AFF"
-        case .black1E1E1E:
-            return "#1E1E1EFF"
-        case .greyEAE5DF:
-            return "#EAE5DFFF"
         case .orangeE49318:
             return "#E49318FF"
+        case .greyAFA08A:
+            return "#AFA08AFF"
+        case .greyEAE5DF:
+            return "#EAE5DFFF"
+        case .greyC8BCAB:
+            return "#C8BCABFF"
+        case .black1E1E1E:
+            return "#1E1E1EFF"
         }
     }
 }
