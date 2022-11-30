fbf47857f0c803d3b2804dae390f075472a99ab5
luminoux <yungzin98@naver.com>
2022-11-30 14:29:32 +0900

    :sparkles: ReservationViewCoordinating 프로토콜 추가

---
+++ Projects/App/Sources/UI/ReservationView/Coodinator/ReservationViewCoordinating.swift
@@ -0,0 +1,13 @@
+//
+//  ReservationViewCoordinating.swift
+//  App
+//
+//  Created by YeongJin Jeong on 2022/11/30.
+//  Copyright © 2022 ZupZup. All rights reserved.
+//
+
+import Foundation
+
+protocol ReservationViewCoordinating {
+    func pushReservationViewController()
+}
---
+++ Projects/App/Sources/UI/ReservationView/ViewController/ReservationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReservationViewController.swift
+//  App
+//
+//  Created by YeongJin Jeong on 2022/11/09.
+//  Copyright © 2022 ZupZup. All rights reserved.
+//
+
+import Foundation
+
+class ReservationViewController: BaseViewController {
+    
+    private var viewModel: HomeViewModel
+    
+    // MARK: Initializer
+    init(viewModel: HomeViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Life-Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
