ed33a0f9f60a7655336bd09d3d8217ac4548bbd9
luminoux <yungzin98@naver.com>
2022-12-09 21:33:44 +0900

    :sparkles: SetInfoViewController -> 버튼 추가

--- Projects/App/Sources/UI/SetInfoView/ViewController/SetInfoViewController.swift
+++ Projects/App/Sources/UI/SetInfoView/ViewController/SetInfoViewController.swift
@@ -12,17 +12,44 @@ class SetInfoViewController: BaseViewController {
     
     private var viewModel: SetInfoViewModel
     
+    private let dismissButton: ZupzupDismissButton = {
+        let button = ZupzupDismissButton()
+        button.layer.cornerRadius = DeviceInfo.screenWidth * 30 / 390 / 2
+        return button
+    }()
+    
+    private let setTimeButton = ZupzupButton(title: "시간 정하기")
+    
     // MARK: Initializer
     init(viewModel: SetInfoViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
         view.backgroundColor = .white
+        setUI()
+
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    weak var coordinator: Coordinator?
+}
 
+extension SetInfoViewController {
+    private func setUI() {
+        view.addSubview(dismissButton)
+        view.addSubview(setTimeButton)
+        
+        dismissButton.snp.makeConstraints { make in
+            make.width.height.equalTo(DeviceInfo.screenWidth * 30 / 390)
+            make.right.equalToSuperview().inset(DeviceInfo.screenWidth * 14 / 390)
+            make.top.equalToSuperview().inset(DeviceInfo.screenHeight * 17 / 844)
+        }
+        
+        setTimeButton.snp.makeConstraints { make in
+            make.width.equalTo(DeviceInfo.screenWidth * 358 / 390)
+            make.height.equalTo(DeviceInfo.screenHeight * 57 / 844)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(DeviceInfo.screenHeight * 34 / 844)
+        }
+    }
 }
