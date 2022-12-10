5442f0d8747c80f27d9e0d96b05e9a2cacbb2e3b
luminoux <yungzin98@naver.com>
2022-12-10 15:26:12 +0900

    :memo: 중간 커밋

--- Projects/App/Sources/UI/SetTimeView/ViewController/SetInfoViewController.swift
+++ Projects/App/Sources/UI/SetTimeView/ViewController/SetInfoViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class SetInfoViewController: BaseViewController {
     
-    private var viewModel: SetInfoViewModel
+    var viewModel: SetInfoViewModel
+    
+    private let dismissButton: ZupzupDismissButton = {
+        let button = ZupzupDismissButton()
+        button.layer.cornerRadius = DeviceInfo.screenWidth * 30 / 390 / 2
+        return button
+    }()
     
     // MARK: Initializer
     init(viewModel: SetInfoViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
-        view.backgroundColor = .red
+        setUI()
+        setButtonTarget()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SetInfoViewController {
+    
+    private func setBackGround() {
+        view.backgroundColor = .white
+    }
+    
+    private func setUI() {
+        view.addSubview(dismissButton)
+        
+        dismissButton.snp.makeConstraints { make in
+            make.width.height.equalTo(DeviceInfo.screenWidth * 30 / 390)
+            make.right.equalToSuperview().inset(DeviceInfo.screenWidth * 14 / 390)
+            make.top.equalToSuperview().inset(DeviceInfo.screenHeight * 45 / 844)
+        }
+    }
+    
+    private func setButtonTarget() {
+        dismissButton.addTarget(self, action: #selector(tapDismissButton), for: .touchUpInside)
+    }
+    
+    @objc
+    func tapDismissButton() {
+        viewModel.dismissViewController()
     }
 }
