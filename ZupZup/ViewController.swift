39b7fe0ae3ab55f304e244f0435f7b917c216d79
luminoux <yungzin98@naver.com>
2022-12-15 16:50:12 +0900

    :tada: Tuist 제거한 Xcode Project 생성

---
+++ ZupZup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZupZup
+//
+//  Created by YeongJin Jeong on 2022/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
