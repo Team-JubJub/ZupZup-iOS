721204aae80363b69cb5b7ecf9149c2664cb2c4f
luminoux <yungzin98@naver.com>
2022-12-23 14:27:51 +0900

    :sparkles: AddReservationDTO 추가

---
+++ Projects/ZupZupNetwork/Sources/AddReservationDTO/AddReservationDTO.swift
@@ -0,0 +1,28 @@
+//
+//  AddReservationDTO.swift
+//  ZupZupNetworkTests
+//
+//  Created by YeongJin Jeong on 2022/12/23.
+//  Copyright © 2022 ZupZup. All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+public struct AddReservationDTO: Codable {
+    @DocumentID var reserveId: String?
+    public var customerName: String
+    public var customerPhone: String
+    public var state: String
+    public var storeId: Int
+    public var visitTime: Int
+    public var cartList: [CartDTO]
+    
+    public struct CartDTO: Codable {
+        public var itemId: Int
+        public var storeId: Int
+        public var amount: Int
+        public var name: String
+        public var salesPrice: Int
+    }
+}
